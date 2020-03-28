3cbc6863918352ec2ec5df0ca174dda13c0be894
Vipin Baswan <f20170429@pilani.bits-pilani.ac.in>
2020-03-29 00:31:19 +0530

    Partially debugged symbol table code

--- symbolTable.h
+++ symbolTable.h
@@ -1,14 +1,5 @@
-/* 
-	GROUP 33
-	Aryan Mehra 2017A7PS0077P
-	Akshit Khanna 2017A7PS0023P
-   	Vipin Baswan 2017A7PS0429P
-   	Swadesh Vaibhav 2017A7PS0030P
-*/
-
 #ifndef SYMBOLTABLE_H_
 #define SYMBOLTABLE_H_
-
 #include "symbolTableDef.h"
 
 void sympush(tableStack *stack, tableStackEle *newNode);
@@ -20,5 +11,7 @@ hashSym *rehash(hashSym *oldTable);
 void initializeHashSym(hashSym *hash_tb);
 symbolTable* initializeSymbolTable(char *str, int lineNumStart, int lineNumEnd);
 void formulation(astNode* astRoot, symbolTable * current);
-
+void printSymbolTable();
+void printSymTableNode(symbolTable *ST);
+void printHashTable(hashSym hashtb);
 #endif
